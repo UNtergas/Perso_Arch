37b08c3f06063c113b0c7a6c913e1f3b0f8e0fe9
UNtergas <mycomputerer135@gmail.com>
2025-01-13 23:19:04 +0100

    update waybar & hyprlock

--- config2/hypr/scripts/ChangeBlur.sh
+++ config2/hypr/scripts/ChangeBlur.sh
@@ -11,7 +11,7 @@ if [ "${STATE}" == "2" ]; then
 	hyprctl keyword decoration:blur:passes 1
  	notify-send -e -u low -i "$notif" "Less blur"
 else
-	hyprctl keyword decoration:blur:size 4
+	hyprctl keyword decoration:blur:size 7
 	hyprctl keyword decoration:blur:passes 2
   	notify-send -e -u low -i "$notif" "Normal blur"
 fi
