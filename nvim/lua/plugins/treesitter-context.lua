15c88e5aa690b1034522117bc06c98e5e252f0a0
UNtergas <mycomputerer135@gmail.com>
2025-03-10 17:29:33 +0100

    nv

--- nvim/lua/plugins/treesitter-context.lua
+++ nvim/lua/plugins/treesitter-context.lua
@@ -5,13 +5,15 @@ return{
     lazy = false,
     enabled = not vim.g.vscode,
     config = function()
-    require'treesitter-context'.setup{
-      enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-      max_lines = 5, -- How many lines the window should span. Values <= 0 mean no limit.
-      line_numbers = true,
-      trim_scope = 'inner', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-      mode = 'topline',  -- Line used to calculate context. Choices: 'cursor', 'topline'
-    }
+      require'treesitter-context'.setup{
+        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+        max_lines = 5, -- How many lines the window should span. Values <= 0 mean no limit.
+        line_numbers = true,
+        trim_scope = 'inner', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+      }
+      vim.keymap.set("n", "[c", function()
+        require("treesitter-context").go_to_context(vim.v.count1)
+      end, { silent = true })
     end,
   }
 }
