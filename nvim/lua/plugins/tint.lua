33b845fefda8b8426fdb76b12eaa759c7eac2d25
UNtergas <mycomputerer135@gmail.com>
2025-03-13 10:00:59 +0100

    Minor document change

---
+++ nvim/lua/plugins/tint.lua
@@ -0,0 +1,20 @@
+return {
+  -- Add tint.nvim
+  {
+    "levouh/tint.nvim",
+    event = "VeryLazy", -- Load when UI is ready
+    config = function()
+      require("tint").setup({
+        tint = -20, -- Adjust darkness of inactive splits
+        saturation = 1, -- Keep colors slightly desaturated
+        highlight_ignore_patterns = { "WinSeparator", "Status.*", "NeoTree.*" }, -- Ignore certain UI elements
+        window_ignore_function = function(winid)
+          local buf = vim.api.nvim_win_get_buf(winid)
+          local buftype = vim.api.nvim_buf_get_option(buf, "buftype")
+          return buftype == "terminal" or buftype == "nofile"
+        end
+      })
+    end
+  }
+}
+
